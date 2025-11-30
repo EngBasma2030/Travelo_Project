77eda8fef2f4ef4744ab29a125e1710c38f5ade0
أهلة <@>
2025-11-30 22:15:47 +0200

    Database

--- Bakend/Travelo_Project/Travelo_Project/Program.cs
+++ Bakend/Travelo_Project/Travelo_Project/Program.cs
@@ -1,4 +1,13 @@
 
+using DomainLayer.Models.Identity;
+using DomainLayer.Models.User;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Persistance;
+using System.Text;
+
 namespace Travelo_Project
 {
     public class Program
@@ -12,23 +21,63 @@ namespace Travelo_Project
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("BaseConnection"));
+
+
+
+            });
+            builder.Services.AddAuthentication(
+                  cobfig =>
+                  {
+                      cobfig.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                      cobfig.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+                  }
+                  ).AddJwtBearer(
+                  options =>
+                  {
+                      options.TokenValidationParameters = new TokenValidationParameters()
+                      {
+                          ValidateIssuer = true,
+                          ValidIssuer = builder.Configuration.GetSection("JwtOptions")["Issuer"],
+                          ValidateAudience = true,
+                          ValidAudience = builder.Configuration.GetSection("JwtOptions")["Audience"],
+                          ValidateLifetime = true,
+                          IssuerSigningKey = new SymmetricSecurityKey((Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JwtOptions")["SecretKey"])))
+
+                      };
+
+
+                  }
+                  );
+            builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+             .AddEntityFrameworkStores<ApplicationDbContext>()
+             .AddDefaultTokenProviders();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+                  // Configure the HTTP request pipeline.
+                  if (app.Environment.IsDevelopment())
+                  {
+                      app.MapOpenApi();
                 app.MapOpenApi();
+               
             }
 
-            app.UseHttpsRedirection();
+                  app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+                  app.UseAuthorization();
+          
+            app.UseRouting();
+            app.UseAuthentication();
+    
 
 
             app.MapControllers();
 
-            app.Run();
-        }
+                  app.Run();
+              }
     }
-}
+    } 
